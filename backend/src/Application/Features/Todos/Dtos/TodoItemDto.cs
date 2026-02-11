d84f3229f9fb8b141e6888de763ef4845868b1b7
nhontran2509 <nhonvn2509@gmail.com>
2026-02-11 11:29:54 +0700

    update anotation

--- backend/src/Application/Features/Todos/Dtos/TodoItemDto.cs
+++ backend/src/Application/Features/Todos/Dtos/TodoItemDto.cs
@@ -2,11 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 namespace Education.Application.Features.Todos.Dtos;
 
-
 public sealed record TodoItemDto(Guid Id, string Title, bool IsDone, DateTimeOffset CreatedAt);
-
-public sealed record CreateTodoRequest([property: Required] [property: MaxLength(200)] string Title);
-
-public sealed record UpdateTodoRequest([property: Required] [property: MaxLength(200)] string Title, bool IsDone);
-
-public sealed record PatchTodoRequest([property: MaxLength(200)] string? Title, bool? IsDone);
+public sealed record CreateTodoRequest([Required][MaxLength(200)] string Title);
+public sealed record UpdateTodoRequest([Required][MaxLength(200)] string Title, bool IsDone);
+public sealed record PatchTodoRequest([MaxLength(200)] string? Title, bool? IsDone);
